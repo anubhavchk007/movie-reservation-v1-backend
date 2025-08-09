6172781e869dffec6a35ad60ada6f5733d2bbde6
anubhavchk007 <anubhav21_ug@ece.nits.ac.in>
2025-08-09 22:41:33 +0530

    just checking

--- src/main/java/com/example/movie_reservation_v1_backend/controller/AuthController.java
+++ src/main/java/com/example/movie_reservation_v1_backend/controller/AuthController.java
@@ -20,6 +20,7 @@ public class AuthController {
 
     @PostMapping("/signup")
     public AuthResponse register(@RequestBody SignUpRequest request) {
+        boolean ok = true;
         return authService.register(request);
     }
 
